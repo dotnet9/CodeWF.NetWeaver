b585d0cdc05bcf714033d60372ac434f23e58f63
Dotnet9(dotnet9.com) <1012434131@qq.com>
2025-12-10 22:58:16 +0800

    add LocalEndPoint  to tcp client;

--- src/CodeWF.NetWrapper/Helpers/TcpSocketClient.cs
+++ src/CodeWF.NetWrapper/Helpers/TcpSocketClient.cs
@@ -1,45 +1,46 @@
-using CodeWF.Log.Core;
-using CodeWF.NetWeaver;
-using CodeWF.NetWeaver.Base;
-using CodeWF.NetWrapper.Commands;
 using System;
 using System.Collections.Concurrent;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading.Tasks;
+using CodeWF.Log.Core;
+using CodeWF.NetWeaver;
+using CodeWF.NetWeaver.Base;
+using CodeWF.NetWrapper.Commands;
 
 namespace CodeWF.NetWrapper.Helpers;
 
 /// <summary>
-/// TCP Socket 客户端类，用于与 TCP 服务器建立连接并进行通信
+///     TCP Socket 客户端类，用于与 TCP 服务器建立连接并进行通信
 /// </summary>
 public class TcpSocketClient
 {
-    private Socket? _client;
     /// <summary>
-    /// 服务端标识，TCP数据接收时保存，用于UDP数据包识别
-    /// </summary>
-    public long SystemId { get; private set; }
-
-    /// <summary>
-    /// 响应命令队列，用于存储接收到的响应命令
+    ///     响应命令队列，用于存储接收到的响应命令
     /// </summary>
     public readonly BlockingCollection<SocketCommand> _responses = new(new ConcurrentQueue<SocketCommand>());
+
+    private Socket? _client;
+
+    /// <summary>
+    ///     服务端标识，TCP数据接收时保存，用于UDP数据包识别
+    /// </summary>
+    public long SystemId { get; private set; }
 
     #region 公开属性
 
     /// <summary>
-    /// 服务标识，用以区分多个服务
+    ///     服务标识，用以区分多个服务
     /// </summary>
     public string? ServerMark { get; private set; }
 
     /// <summary>
-    /// 获取或设置服务器IP地址
+    ///     获取或设置服务器IP地址
     /// </summary>
     public string? ServerIP { get; private set; }
-    
+
     /// <summary>
-    /// 获取或设置服务器端口号
+    ///     获取或设置服务器端口号
     /// </summary>
     public int ServerPort { get; private set; }
 
@@ -50,7 +51,7 @@ public class TcpSocketClient
     public bool IsRunning { get; set; }
 
     /// <summary>
-    /// 发送时间
+    ///     发送时间
     /// </summary>
     private DateTime _sendTime;
 
@@ -66,31 +67,34 @@ public class TcpSocketClient
     public DateTime ReceiveTime { get; set; }
 
 
-
-
     /// <summary>
-    /// 获取或设置心跳发送时间
+    ///     获取或设置心跳发送时间
     /// </summary>
     public DateTime SendHeartbeatTime { get; set; }
 
     /// <summary>
-    /// 获取或设置心跳响应时间
+    ///     获取或设置心跳响应时间
     /// </summary>
     public DateTime ResponseHeartbeatTime { get; set; }
+
+    /// <summary>
+    /// 本地端点连接信息
+    /// </summary>
+    public string? LocalEndPoint { get; set; }
 
     #endregion
 
     #region 公开接口
 
-
     /// <summary>
-    /// 异步连接到TCP服务器
+    ///     异步连接到TCP服务器
     /// </summary>
     /// <param name="serverMark">服务器标识</param>
     /// <param name="serverIP">服务器IP地址</param>
     /// <param name="serverPort">服务器端口号</param>
     /// <returns>连接结果，包含是否成功和错误信息</returns>
-    public async Task<(bool IsSuccess, string? ErrorMessage)> ConnectAsync(string serverMark, string serverIP, int serverPort)
+    public async Task<(bool IsSuccess, string? ErrorMessage)> ConnectAsync(string serverMark, string serverIP,
+        int serverPort)
     {
         ServerMark = serverMark;
         ServerIP = serverIP;
@@ -106,36 +110,37 @@ public class TcpSocketClient
 
             await ListenForServerAsync();
             await CheckResponseAsync();
+
+            LocalEndPoint = _client.LocalEndPoint?.ToString();
             return (IsSuccess: true, ErrorMessage: null);
         }
         catch (Exception ex)
         {
             IsRunning = false;
-            Logger.Error($"{ServerMark} 连接异常", ex, uiContent: $"{ServerMark} 连接异常，详细信息请查看日志文件");
+            LocalEndPoint = null;
+            Logger.Error($"{ServerMark} 连接异常", ex, $"{ServerMark} 连接异常，详细信息请查看日志文件");
             return (IsSuccess: false, ErrorMessage: $"{ServerMark} 连接异常，异常信息：{ex.Message}");
         }
     }
 
     /// <summary>
-    /// 停止TCP客户端
+    ///     停止TCP客户端
     /// </summary>
     public void Stop()
     {
         IsRunning = false;
         _client?.CloseSocket();
+        LocalEndPoint = null;
     }
 
     /// <summary>
-    /// 异步发送命令到服务器
+    ///     异步发送命令到服务器
     /// </summary>
     /// <param name="command">要发送的命令对象</param>
     /// <exception cref="Exception">当客户端未连接时抛出</exception>
     public async Task SendCommandAsync(INetObject command)
     {
-        if (!IsRunning || !_client.Connected)
-        {
-            throw new Exception($"{ServerMark} 未连接，无法发送命令");
-        }
+        if (!IsRunning || !_client.Connected) throw new Exception($"{ServerMark} 未连接，无法发送命令");
 
         var buffer = command.Serialize(SystemId);
         await _client!.SendAsync(buffer);
@@ -146,19 +151,16 @@ public class TcpSocketClient
     #region 连接TCP、接收数据
 
     /// <summary>
-    /// 监听服务器发送的数据
+    ///     监听服务器发送的数据
     /// </summary>
     private async Task ListenForServerAsync()
     {
         while (IsRunning)
-        {
             try
             {
                 var (success, buffer, headInfo) = await _client!.ReadPacketAsync();
-                if (!success)
-                {
-                    break;
-                }
+                if (!success) break;
+
                 ReceiveTime = DateTime.Now;
                 SystemId = headInfo!.SystemId;
                 _responses.Add(new SocketCommand(headInfo, buffer, _client));
@@ -170,31 +172,24 @@ public class TcpSocketClient
             }
             catch (Exception ex)
             {
-                if (IsRunning)
-                {
-                    Logger.Error($"{ServerMark} 处理接收数据异常", ex, $"{ServerMark} 处理接收数据异常，详细信息请查看日志文件");
-                }
+                if (IsRunning) Logger.Error($"{ServerMark} 处理接收数据异常", ex, $"{ServerMark} 处理接收数据异常，详细信息请查看日志文件");
+
                 break;
             }
-        }
     }
 
     /// <summary>
-    /// 检查响应命令队列
+    ///     检查响应命令队列
     /// </summary>
     private async Task CheckResponseAsync()
     {
-        while (!IsRunning)
-        {
-            await Task.Delay(TimeSpan.FromMilliseconds(10));
-        }
+        while (!IsRunning) await Task.Delay(TimeSpan.FromMilliseconds(10));
 
         while (IsRunning)
         {
             while (_responses.TryTake(out var command, TimeSpan.FromMilliseconds(10)))
-            {
                 await EventBus.EventBus.Default.PublishAsync(command);
-            }
+
             await Task.Delay(TimeSpan.FromMilliseconds(10)); // 添加一个小延迟，避免CPU占用过高
         }
     }
